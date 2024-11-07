44693cbd3912676fda28de134bd73f36b46a87fd
Tazah <finnbeifong@gmail.com>
2024-11-07 19:12:08 +0100

    get image recognition working with correct schema

--- ai/test.py
+++ ai/test.py
@@ -1,14 +1,27 @@
-from openai import OpenAI
-from ai.response_format import Factura
+import openai
+from response_format import Factura
+import base64
+from dotenv import load_dotenv
+import os
 
-client = OpenAI()
+# Load environment variables from .env file
+load_dotenv()
 
-image_url = "test" # ToDo: upload the img somewhere to use it
+openai_api_key = os.getenv('OPENAI_API_KEY')
 
-with open('system_prompt.txt', 'r') as file:
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
+
+image_path = "./test/test_small.png"
+base64_image = encode_image(image_path)
+
+with open('./ai/system_prompt.txt', 'r') as file:
   system_prompt = file.read()
 
-response = client.chat.completions.create(
+openai.api_key = openai_api_key
+
+response = openai.ChatCompletion.create(
   model="gpt-4o-mini",
   messages=[
     {
@@ -21,7 +34,7 @@ response = client.chat.completions.create(
         {
           "type": "image_url",
           "image_url": {
-            "url": image_url
+            "url": f"data:image/jpeg;base64,{base64_image}"
           }
         }
       ]
@@ -32,5 +45,14 @@ response = client.chat.completions.create(
   top_p=1,
   frequency_penalty=0,
   presence_penalty=0,
-  response_format=Factura
+  response_format={
+           'type': 'json_schema',
+           'json_schema': 
+              {
+                "name":"factura", 
+                "schema": Factura.model_json_schema()
+              }
+         } 
 )
+
+print(response)
