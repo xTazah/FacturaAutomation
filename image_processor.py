ce9e075c216ded6d83d0dacc6fe2008212621687
Tazah <finnbeifong@gmail.com>
2024-11-08 15:20:40 +0100

    process factura with openai api

--- image_processor.py
+++ image_processor.py
@@ -1,9 +1,22 @@
+import base64
+from io import BytesIO
+import json
 import threading
-import time
+import openai
+import os
+import dotenv
 from queue import Queue, Empty
 from PIL import Image, PngImagePlugin
 import cv2
 import numpy as np
+from pydantic import ValidationError
+from ai.response_format import Factura
+from exceptions import NoFacturaFoundException
+
+dotenv.load_dotenv()
+
+openai_api_key = os.getenv('OPENAI_API_KEY')
+openai.api_key = openai_api_key
 
 class ImageProcessorThread(threading.Thread):
     def __init__(self, image_queue: Queue, shutdown_event):
@@ -20,7 +33,6 @@ class ImageProcessorThread(threading.Thread):
         search_params = dict(checks=50) #ToDo check if other number of checks is better (performance and accuracy)
 
         self.flann = cv2.FlannBasedMatcher(index_params, search_params)
-
 
         self.template_img =  cv2.cvtColor(cv2.imread('test/blank_factura.png'), cv2.COLOR_BGR2GRAY)
         self.template_keypoints, self.template_descriptors = self.sift.detectAndCompute(self.template_img, None)
@@ -44,24 +56,34 @@ class ImageProcessorThread(threading.Thread):
                 continue
 
     def process_image(self, image_path):
-        img = Image.open(image_path)
+        img = Image.open(image_path).copy()
         
         # update metadata to indicate processing started
         metadata = PngImagePlugin.PngInfo()
         metadata.add_text("status", "Processing")
         img.save(image_path, "PNG", pnginfo=metadata)
         
-        print(f"Processing image: {image_path}")
+        try:
+            img = self.preprocess_image(img)
 
-        img_cv = cv2.imread(image_path)
+            factura:Factura = self.send_to_openai(img)
 
-        img = self.preprocess_image(img_cv)
-        #img.show()
+            #ToDo: write to google sheets api
 
-        # update metadata
-        metadata = PngImagePlugin.PngInfo()
-        metadata.add_text("status", "Completed")
-        img.save(image_path, "PNG", pnginfo=metadata)
+            # update metadata
+            metadata.add_text("status", "Completed")
+            metadata.add_text("factura", factura.InvoiceNumber)
+            img.save(image_path, "PNG", pnginfo=metadata)
+
+        except NoFacturaFoundException:
+            metadata.add_text("factura", "False")
+            metadata.add_text("status", "Completed")
+            img.save(image_path, "PNG", pnginfo=metadata)
+
+        except Exception as e:
+            print(e)
+            metadata.add_text("status", "Error")
+            img.save(image_path, "PNG", pnginfo=metadata)
 
         print(f"Processed image: {image_path}")
 
@@ -79,9 +101,8 @@ class ImageProcessorThread(threading.Thread):
         """
         print("Preprocessing")
         
-
         # conver to grayscale for beter matching
-        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img_gray = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2GRAY)
 
         # detect keypoints and descriptors for image
         img_keypoints, image_descriptors = self.sift.detectAndCompute(img_gray, None)
@@ -119,11 +140,69 @@ class ImageProcessorThread(threading.Thread):
 
             # warps perspective 
             M_warp = cv2.getPerspectiveTransform(dst, dst_corners)
-            factura_warped = cv2.warpPerspective(img, M_warp, (target_width, target_height))
+            factura_warped = cv2.warpPerspective(np.array(img), M_warp, (target_width, target_height))
 
             #crop out everything thats not needed to reduce img size (vision tokens are expensive openai angrE)
             factura_cropped = factura_warped[125:target_height - 175, 0:target_width]
 
-            return Image.fromarray(cv2.cvtColor(factura_cropped, cv2.COLOR_BGR2RGB))
+            return Image.fromarray(factura_cropped)
         else:
-            raise Exception("No factura found in the image.")
+            raise NoFacturaFoundException("No factura found in the image.")
+        
+    def encode_image(self,img):
+        # saves to buffer and then encodes to base64
+        buffered = BytesIO()
+        img.save(buffered, format="PNG")
+        return base64.b64encode(buffered.getvalue()).decode('utf-8')
+
+    def send_to_openai(self,cropped_image):
+        base64_image = self.encode_image(cropped_image)
+        with open('./ai/system_prompt.txt', 'r') as file:
+            system_prompt = file.read()
+
+        response = openai.ChatCompletion.create(
+            model="gpt-4o-mini",
+            messages=[
+                {
+                    "role": "system",
+                    "content": system_prompt 
+                },
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": f"data:image/jpeg;base64,{base64_image}"
+                            }
+                        }
+                    ]
+                }
+            ],
+            temperature=0.63,
+            max_tokens=2048,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            response_format= {
+                'type': 'json_schema',
+                'json_schema': 
+                {
+                    "name":"factura", 
+                    "schema": Factura.model_json_schema()
+                }
+            }  
+        )
+        print(response)
+
+        content_json_str = response["choices"][0]["message"]["content"]
+
+        content_dict = json.loads(content_json_str)
+
+        try:
+            factura = Factura(**content_dict)
+            print(factura)
+        except ValidationError as e:
+            print("Error casting to Factura:", e)
+
+        return factura
